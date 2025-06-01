641e276ddb0e2fdadd8596db3d4e198d3f60c469
Md. Akram Ul Hasan <akramulhasan.dev@gmail.com>
2025-06-01 23:47:21 +0600

    Initial Commit

---
+++ BDCurrencyDetector/BDCurrencyDetectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  BDCurrencyDetectorApp.swift
+//  BDCurrencyDetector
+//
+//  Created by Akram Ul Hasan on 1/6/25.
+//
+
+import SwiftUI
+
+@main
+struct BDCurrencyDetectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BDCurrencyDetector/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BDCurrencyDetector
+//
+//  Created by Akram Ul Hasan on 1/6/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
