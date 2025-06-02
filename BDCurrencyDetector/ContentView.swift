802ccc43ceb4ee934429bc46899b5af01393e200
Md. Akram Ul Hasan <akramulhasan.dev@gmail.com>
2025-06-02 11:55:08 +0600

    added a simple UI and predict amount

--- BDCurrencyDetector/ContentView.swift
+++ BDCurrencyDetector/ContentView.swift
@@ -7,31 +7,102 @@
 
 import SwiftUI
 import CoreML
+import PhotosUI
+
+struct ProbabilityListView: View {
+    
+    let probs: [Dictionary<String, Double>.Element]
+    
+    var body: some View {
+        List(probs, id: \.key) { (key, value) in
+            
+            HStack {
+                Text(key)
+                Text(NSNumber(value: value), formatter: NumberFormatter.percentage)
+            }
+        }
+    }
+}
 
 struct ContentView: View {
     
-    let model: BDCurrencyDetector?
+    let model = try! BDCurrencyDetector(configuration: MLModelConfiguration())
+    @State private var probs: [String: Double] = [: ]
+    @State private var selectedPhotoItem: PhotosPickerItem? = nil
+    @State private var uiImage: UIImage? = UIImage(named: "500Taka")
+    @State private var isCameraSelected: Bool = false
     
-    init() {
-        do {
-            let config = MLModelConfiguration()
-            model = try BDCurrencyDetector(configuration: config)
-        } catch {
-            print("Failed to load model: \(error)")
-            model = nil
+    var sortedProbs: [Dictionary<String, Double>.Element] {
+        
+        let probsArray = Array(probs)
+        return probsArray.sorted { lhs, rhs in
+            lhs.value > rhs.value
         }
     }
     
     var body: some View {
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            if let uiImage {
+                Image(uiImage: uiImage)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 300, height: 300)
+            }
+            
+            HStack {
+                
+                PhotosPicker(selection: $selectedPhotoItem, matching: .images) {
+                    Text("Select a Photo")
+                }
+                
+                Button("Camera") {
+                    isCameraSelected = true
+                }.buttonStyle(.bordered)
+                    
+            }
+            
+            
+            Button("Predict") {
+                
+                guard let resizedImage = uiImage?.resizeTo(to: CGSize(width: 299, height: 299)) else {
+                    return
+                }
+                guard let buffer = resizedImage.toCVPixelBuffer() else { return }
+                
+                do {
+                    let result = try model.prediction(image: buffer)
+                    probs = result.targetProbability
+                } catch {
+                    print(error.localizedDescription)
+                }
+                
+            }.buttonStyle(.borderedProminent)
+            
+            ProbabilityListView(probs: sortedProbs)
         }
+        .onChange(of: selectedPhotoItem, perform: { selectedPhotoItem in
+            selectedPhotoItem?.loadTransferable(type: Data.self, completionHandler: { result in
+                switch result {
+                    case .success(let data):
+                        if let data {
+                            guard let img = UIImage(data: data) else { return }
+                            uiImage = img
+                        }
+                    case .failure(let error):
+                        print(error.localizedDescription)
+                }
+            })
+        })
+
+        .sheet(isPresented: $isCameraSelected, content: {
+            ImagePicker(image: $uiImage, sourceType: .camera)
+        })
         .padding()
     }
 }
+
 
 #Preview {
     ContentView()
